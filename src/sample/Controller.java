510c32d483147a1a8341d55123248754b057f123
Kevich <kevich.mz@gmail.com>
2015-12-15 14:32:23 +0300

    Add private items to Controller class

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,9 +1,15 @@
 package sample;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
+import javafx.fxml.Initializable;
+import javafx.scene.control.*;
 
-public class Controller {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class Controller implements Initializable{
 
     @FXML
     private Button buttonFind;
@@ -11,5 +17,36 @@ public class Controller {
     @FXML
     private Button buttonRefresh;
 
+    @FXML
+    private Label labelSelectCurency;
 
+    @FXML
+    private Label labelExchangeRates;
+
+    @FXML
+    private Label labelBanks;
+
+    @FXML
+    private Label labelStatusBar;
+
+    private ObservableList<String> list = FXCollections.observableArrayList(
+            "USD", "EUR", "RUB"
+    );
+
+    @FXML
+    private ComboBox<String> comboBox;
+
+    @FXML
+    private MenuBar menuBar;
+
+    @FXML
+    private void comboBoxAction(){
+
+        System.err.println("choiceBoxAction");
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        comboBox.setItems(list);
+    }
 }
